38501686623cc55f2d1747c39fde483905f243ff
maroo0011001-design <maroo0011001@gmail.com>
2026-05-04 04:14:58 +0300

    Update App.tsx

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,4 +1,4 @@
-'use client';
+ 'use client';
 import React, { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword, onAuthStateChanged, signOut, type User } from 'firebase/auth';
 
